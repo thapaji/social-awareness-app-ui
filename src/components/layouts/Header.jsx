cb686332e480d3eb4d1f879d4fba0107087347f7
Sujan <mail.thapasujan@gmail.com>
2024-07-05 14:20:46 +1000

    Some design changes to the homepage

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,26 +1,58 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Nav, Navbar, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <>
-      <Row>
-        <Col>Cause Chronicle</Col>
-        <Col>
-          <Col>
-            <ul>
-              <li>Following</li>
-              <li>New</li>
-              <li>Popular</li>
-            </ul>
-          </Col>
-          <Col>Menu</Col>
+    <div className="header">
+      <Row className="p-4">
+        <Col lg={1}>
+          <Link className="navbar-brand">Cause Chronicle</Link>
         </Col>
-        <Col>Name</Col>
+        <Col className="px-4">
+          <Row>
+            <Col className="px-4 d-flex justify-content-between">
+              <Col className="d-grid">
+                <Button className="rounded" variant="light">
+                  Following
+                </Button>
+              </Col>
+              <Col className="d-grid">
+                {" "}
+                <Button className="rounded" variant="light">
+                  New
+                </Button>
+              </Col>
+              <Col className="d-grid">
+                <Button className="rounded" variant="light">
+                  Popular
+                </Button>
+              </Col>
+            </Col>
+            <Col lg={1}>
+              <Navbar expand="" className="bg-body-tertiary">
+                <Container>
+                  {/* <Navbar.Brand href="#home">Menu</Navbar.Brand> */}
+                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                  <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                      <Nav.Link href="#home">Home</Nav.Link>
+                      <Nav.Link href="#link">Link</Nav.Link>
+                    </Nav>
+                  </Navbar.Collapse>
+                </Container>
+              </Navbar>
+            </Col>
+          </Row>
+        </Col>
+        <Col lg={1}>
+          {" "}
+          <Link className="navbar-brand">Name</Link>
+        </Col>
       </Row>
       <Row>
-        <input type="text" />
+        <Form.Control type="text" style={{ width: "50%", margin: "auto" }} />
       </Row>
-    </>
+    </div>
   );
 };
